86b0009d660a62d57d8be76fd6058aeddd47351d
Syed Anas Bukhari <anas14529@gmail.com>
2025-01-06 08:42:24 +0500

    Work done.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,32 +1,65 @@
-$(document).ready(function() {
-    if ($('.brand-logos').length) {
-      $('.brand-logos').slick({
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: false,
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 992, // lg screen
-            settings: {
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 768, // md screen
-            settings: {
-              slidesToShow: 2
-            }
-          },
-          {
-            breakpoint: 576, // sm screen
-            settings: {
-              slidesToShow: 2
-            }
+$(document).ready(function () {
+  if ($('.brand-logos').length) {
+    $('.brand-logos').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: false,
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 992, // lg screen
+          settings: {
+            slidesToShow: 3
           }
-        ]
-      });
-    }
-  });
+        },
+        {
+          breakpoint: 768, // md screen
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 576, // sm screen
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+    });
+  }
+
+  if ($('.testimonials-slider').length) {
+    $('.testimonials-slider').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      appendDots: $('.ts-dots')
+    });
+  }
+  if ($('.vpi-cont').length) {
+    $('.vpi-cont').on('click', function () {
+      // Find the closest video related to the clicked icon
+      let video = $(this).closest('.ip-vid-cont').find('video');
+
+      if (video.length) {
+        video[0].play();
+
+        // Hide the icon when the video starts playing
+        video.on('play', () => {
+          $(this).hide();
+        });
+
+        // Show the icon when the video is paused
+        video.on('pause', () => {
+          $(this).show();
+        });
+      }
+    });
+  }
+
+});
